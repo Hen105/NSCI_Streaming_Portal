7a85f9ec71f73f50690e18aa7819c3744ead6f0b
Hen105 <henryfiely@gmail.com>
2024-06-30 20:36:31 -0500

    updated gitignore and config

--- src/config.example.js
+++
@@ -1,7 +0,0 @@
-export default {
-  accountId: '', // <-- find this from BoxCast Support
-  channelId: '', // <-- find this on your boxcast.tv global account page
-  channelFilter: (c) => {
-    return true  // <-- can use this to exclude certain channels from left nav
-  }
-}
---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+  accountId: 'a9rrszx4arobhocjb4yl', // <-- find this from BoxCast Support
+  channelId: 'zuafosdxwygexqa9cmrm', // <-- find this on your boxcast.tv global account page
+  channelFilter: (c) => {
+    return true  // <-- can use this to exclude certain channels from left nav
+  }
+}
